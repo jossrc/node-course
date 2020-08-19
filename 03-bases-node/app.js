f9d9779f9c66a4e7d58d763ef7f062ae334576d2
jossrc <kuro.rc10@gmail.com>
2020-08-19 16:22:22 -0500

    Utilizando el paquete fs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,19 @@
-let base = 2;
+const fs = require("fs");
 
-for (let i = 1; i <= 10; i++) {
-  let result = base * i;
-  console.log(`${base} * ${i} = ${result}`);
-}
+let base = 3;
+
+const createMultiplicationTable = () => {
+  let data = '';
+  for (let i = 1; i <= 10; i++) {
+    let result = base * i;
+    data += `${base} * ${i} = ${result}\n`;
+  }
+  return data;
+};
+
+const path = `tables/mult-${base}.txt`;
+
+fs.writeFile(path, createMultiplicationTable(), (err) => {
+  if (err) throw err;
+  console.log(`El archivo mult-${base}.txt a sido creado`);
+});
