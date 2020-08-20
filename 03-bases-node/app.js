aad5170bad40ed9521ab56d7296cd7b66b36d045
jossrc <kuro.rc10@gmail.com>
2020-08-20 11:05:56 -0500

    Recibir información desde la línea de comandos

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,8 +1,20 @@
-const {createFile: createMultiplicationFile} = require('./multiplication/multiplication');
+const {createFile} = require('./multiplication/multiplication');
 
-let base = 5;
+// proccess -> Pertenece a Node
+// console.log(process);
+
+// proccess.argv -> Obtiene las variables que se pasan por la terminal
+// console.log(process.argv);
+
+let argv = process.argv;
+let parameters = argv[2];
+let base = parameters.split('=')[1];
+
 let path = `tables/mult-${base}.txt`;
 
-createMultiplicationFile(base, path)
+// Trabajar con parámetros por la terminal es un dolor de cabeza 
+// Por lo que se recomienda usar "yargs"
+
+createFile(base, path)
   .then( file => console.log(`Archivo creado : ${file}`) )
   .catch( err => console.log(`ERROR : ${err}`))
