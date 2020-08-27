aaf806be3ffa73e49674684867da59a01cbb4556
jossrc <kuro.rc10@gmail.com>
2020-08-27 10:21:42 -0500

    Creando un archivo .hbs y renderizandolo en pantalla

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -8,27 +8,9 @@ app.use(express.static(__dirname+'/public'));
 // Express HBS engine
 app.set('view engine', 'hbs');
 
-// Por defecto muestra siempre el index.html
-// En caso de tener un About este debera estar escrito con su formato "/about.html"
-
-// La petición se hará cuando sea de tipo GET en "/"
-// app.get('/', (req, res) => {
-//   res.send('Hello World');
-// }); 
-// Debemos tener cuidado con este ya que se combina con el contenido publico
-
-
-// La petición se hará cuando sea de tipo GET en "/data"
-app.get('/data', (req, res) => {
-  let output = {
-    name: 'José',
-    age: '21',
-    url: req.url,
-  };
-
-  // Con express evitamos configurar el header
-  res.send(output);
-});
+app.get('/', (req, res) => {
+  res.render('home');
+})
 
 app.listen(3000, ()=> {
     console.log('Escuchando peticiones en el puerto 3000');
