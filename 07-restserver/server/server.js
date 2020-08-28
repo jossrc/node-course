9a41c5cb7fa1fd6e7efe1516ac01b255a17bff96
jossrc <kuro.rc10@gmail.com>
2020-08-28 12:28:18 -0500

    Probando el body-parser

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+// Para procesar peticiones x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended:false }))
+
+// Para procesar peticiones json
+app.use(bodyParser.json());
 
 app.get('/user', (req, res) => {
   res.json('getUser');
 });
 
 app.post('/user/:id', (req, res) => {
-  let { id } = req.params;
+  let body = req.body;
 
-  res.json({
-    id: id,
-    name: 'Katt',
-  });
+  res.json({body});
+
 });
 
 app.put('/user', (req, res) => {
