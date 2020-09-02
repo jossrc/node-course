3e0c5356c8c7ee086117be6f97293d438d9a81c7
jossrc <kuro.rc10@gmail.com>
2020-09-02 11:12:29 -0500

    Configurando el login del usuario

--- 07-restserver/server/routes/login.js
+++ 07-restserver/server/routes/login.js
@@ -5,8 +5,39 @@ const User = require('../models/user');
 const app = express();
 
 app.post('/login', (req, res) => {
-  res.json({
-    ok: true,
+  let body = req.body;
+
+  User.findOne({ email: body.email }, (err, userDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err,
+      });
+    }
+
+    if (!userDB) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: '(Usuario) o contraseña incorrectos',
+        },
+      });
+    }
+
+    if (!bcrypt.compareSync(body.password, userDB.password)) {
+      return res.status(400).json({
+        ok: false,
+        err: {
+          message: 'Usuario o (contraseña) incorrectos',
+        },
+      });
+    }
+
+    res.json({
+      ok: true,
+      user: userDB,
+      token: '123',
+    });
   });
 });
 
