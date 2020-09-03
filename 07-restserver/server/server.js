7829e679b1269b0b1396c3400f0f4d9f9545d05e
jossrc <kuro.rc10@gmail.com>
2020-09-03 11:53:31 -0500

    Obteniendo el Api Key y API Secret de google - Creando un index.html y resolviendo las rutas con el path.resolve()

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -3,6 +3,7 @@ require('./config/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path'); // Permite armar una ruta
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Para procesar peticiones json
 app.use(bodyParser.json());
+
+// Habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 // Configuración global de rutas
 app.use(require('./routes/index'));
