5e0d8640aead0373dd5916af581d4b8e5fa96ff0
jossrc <kuro.rc10@gmail.com>
2020-09-03 15:26:27 -0500

    Creando un usuario personalizado en base a las credenciales de Google

--- 07-restserver/server/routes/login.js
+++ 07-restserver/server/routes/login.js
@@ -56,27 +56,96 @@ app.post('/login', (req, res) => {
 
 // Configuraciones de Google
 
-async function verify(token) {
+const verifyGoogleToken = async (token) => {
   const ticket = await client.verifyIdToken({
     idToken: token,
     audience: process.env.CLIENT_ID,
   });
   const payload = ticket.getPayload();
 
-  console.log(payload.name);
-  console.log(payload.email);
-  console.log(payload.picture);
+  return {
+    name: payload.name,
+    email: payload.email,
+    img: payload.picture,
+    google: true,
+  };
+};
 
-}
-
-app.post('/google', (req, res) => {
+app.post('/google', async (req, res) => {
   let token = req.body.idtoken;
 
-  verify(token);
+  let googleUser = await verifyGoogleToken(token).catch((e) =>
+    res.status(403).json({
+      ok: false,
+      err: e,
+    })
+  );
 
-  res.json({
-    token,
+  User.findOne({ email: googleUser.email }, (err, userDB) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        err,
+      });
+    }
+
+    if (userDB) {
+      if (userDB.google === false) {
+        return res.status(400).json({
+          ok: false,
+          err: {
+            message: 'Debe de usar su auntenticación normal',
+          },
+        });
+      } else {
+        let token = jwt.sign(
+          {
+            user: userDB,
+          },
+          process.env.SEED,
+          { expiresIn: process.env.TOKEN_EXPIRATION }
+        );
+
+        return res.json({
+          ok: true,
+          user: userDB,
+          token,
+        });
+      }
+    } else {
+      // Si el usuario no existe en nuestra BD
+      let user = new User();
+      user.name = googleUser.name,
+      user.email = googleUser.email,
+      user.img = googleUser.img,
+      user.google = googleUser.google,
+      user.password = ':)';
+
+      user.save((err, userDB) => {
+        if (err) {
+          return res.status(500).json({
+            ok: false,
+            err,
+          });
+        }
+
+        let token = jwt.sign(
+          {
+            user: userDB,
+          },
+          process.env.SEED,
+          { expiresIn: process.env.TOKEN_EXPIRATION }
+        );
+
+        res.json({
+          ok: true,
+          user: userDB,
+          token,
+        });
+      });
+    }
   });
+
 });
 
 module.exports = app;
