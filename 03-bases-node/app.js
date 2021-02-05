3997961a3eb934c930908229d1e5a2bbeabd0f02
jossrc <kuro.rc10@gmail.com>
2021-02-05 15:59:22 -0500

    Inicio - Nueva versión

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,22 +1,12 @@
-const { argv } = require('./config/yargs');
-const colors = require('colors');
-const { createFile, listTable } = require('./multiplication/multiplication');
+console.clear();
+const base = 5
 
-let command = argv._[0];
-let base = argv.base;
-let limit = argv.limit;
-let path = `tables/mult-${base}.txt`;
+console.log("========================")
+console.log(`   Tabla del: ${base}   `)
+console.log("========================\n")
 
-switch (command) {
-  case 'list':
-    listTable(base, limit);
-    break;
-  case 'create':
-    createFile(base, limit, path)
-      .then((file) => console.log('Archivo creado : ' + `${file}`.blue))
-      .catch((err) => console.log(`ERROR : ${err}`.red));
-    break;
-  default:
-    console.log('Command not recognized'.cyan);
-    break;
+for (let i = 1; i <= 10; i++) {
+  console.log(`${base} x ${i} = ${5*i}`);  
 }
+
+
