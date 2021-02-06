e5cb46a6751e7dfd13df0472879a843f4639c01a
jossrc <kuro.rc10@gmail.com>
2021-02-06 11:14:52 -0500

    Configuración de yargs independientemente

--- 03-bases-node/config/yargs.js
+++ 03-bases-node/config/yargs.js
@@ -1,25 +1,21 @@
-const options = {
-  base: {
-    demand: true,
-    alias: 'b',
-    describe: 'Indica la base del número a multiplicar',
-  },
-  limit: {
-    default: 10,
-    alias: 'l',
-    describe: 'Indica el límite de la tabla',
-  },
-};
-
 const argv = require('yargs')
-  .command('list', 'Imprime en consola la tabla de multiplicar', options)
-  .command(
-    'create',
-    'Crea un archivo .txt con la tabla de multiplicar',
-    options
-  )
-  .help().argv;
+  .option('b', {
+    alias: 'base',
+    type: 'number',
+    demandOption: true,
+    describe: 'Establece la base de la tabla de multiplicar'
+  })
+  .option('l', {
+    alias: 'list',
+    type: 'boolean',
+    default: false,
+    describe: 'Muestra la tabla creada en consola'
+  })
+  .check((argv, options) => {
+    if (isNaN(argv.b)) throw 'La base tiene que ser un número';
+    return true;
+  }).argv;
 
 module.exports = {
-    argv
+  argv
 }
