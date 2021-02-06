87e4bc33ddc60b08bafe1d3324dbe2718d9fd873
jossrc <kuro.rc10@gmail.com>
2021-02-06 10:58:31 -0500

    Agregando la opción de listar

--- 03-bases-node/helpers/multiply.js
+++ 03-bases-node/helpers/multiply.js
@@ -1,26 +1,26 @@
 const fs = require('fs');
 
-const createFile = async (base = 1) => {
+const createFile = async (base = 1, requireListing) => {
   try {
     let output = '';
-
-    console.log('========================');
-    console.log('   Tabla del:', base);
-    console.log('========================\n');
 
     for (let i = 1; i <= 10; i++) {
       output += `${base} x ${i} = ${5 * i}\n`;
     }
 
-    console.log(output);
+    if (requireListing) {
+      console.log('========================');
+      console.log('   Tabla del:', base);
+      console.log('========================\n');
+
+      console.log(output);
+    }
 
     let path = `tabla-${base}.txt`;
     fs.writeFileSync(path, output);
 
     return path;
-
   } catch (error) {
-
     throw error;
   }
 };
