bd217bac5bdeb4a5b1c5176a599763b76d13b786
jossrc <kuro.rc10@gmail.com>
2021-02-06 11:15:15 -0500

    Refactorizando el app.js

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,31 +1,7 @@
 const { createFile } = require('./helpers/multiply');
-const argv = require('yargs')
-  .option('b', {
-    alias: 'base',
-    type: 'number',
-    demandOption: true,
-  })
-  .option('l', {
-    alias: 'list',
-    type: 'boolean',
-    default: false,
-  })
-  .check((argv, options) => {
-    if (isNaN(argv.b)) throw 'La base tiene que ser un número';
-    return true;
-  }).argv;
+const { argv } = require('./config/yargs');
 
 console.clear();
-
-console.log(argv);
-
-console.log('Base : yargs', argv.base);
-console.log('Listar : yargs', argv.list);
-
-//const base = 5;
-
-// const [, , arg3 = 'base=5'] = process.argv;
-// const [, base = 5] = arg3.split('=');
 
 createFile(argv.base, argv.list)
   .then((file) => console.log(file, ' creado'))
