e1e04c895963201cd1e824ee2ffc333904258c79
jossrc <kuro.rc10@gmail.com>
2021-02-06 10:06:44 -0500

    Probando yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,11 +1,20 @@
 const { createFile } = require('./helpers/multiply');
+const argv = require('yargs').argv;
 
 console.clear();
+
+console.log(process.argv);
+console.log(argv);
+
+console.log('Base : yargs', argv.base);
+
 //const base = 5;
 
-const [, , arg3 = 'base=5'] = process.argv;
-const [, base = 5] = arg3.split('=');
+// const [, , arg3 = 'base=5'] = process.argv;
+// const [, base = 5] = arg3.split('=');
 
+/*
 createFile(base)
   .then((file) => console.log(file, ' creado'))
   .catch((err) => console.log(err));
+*/
