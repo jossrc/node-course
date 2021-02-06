7c5bd5ecd0b3d3e9663716ce43cb246bdee9e160
jossrc <kuro.rc10@gmail.com>
2021-02-06 12:48:58 -0500

    Agregando un docstring y actualizando el createFile

--- 03-bases-node/helpers/multiply.js
+++ 03-bases-node/helpers/multiply.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
 const colors = require('colors');
 
-const createFile = async (base = 1, requireListing) => {
+/**
+ * Crea un archivo que contiene la tabla de multiplicación
+ * según la base y el límite que se le establesca.
+ * El retorno es una Promesa con el path donde se ubicó el archivo.
+ * @param {number} base Base de la tabla de multiplicar
+ * @param {number} limit Límite de la tabla de multiplicar
+ * @param {boolean} requireListing Visualizar el resultado en la consola
+ */
+const createFile = async (base, limit ,requireListing) => {
   try {
     let output = '';
 
-    for (let i = 1; i <= 10; i++) {
-      output += `${base} ${'x'.bold.cyan} ${i} ${'='.bold.cyan} ${5 * i}\n`;
+    for (let i = 1; i <= limit; i++) {
+      output += `${base} x ${i} = ${5 * i}\n`;
     }
 
     if (requireListing) {
