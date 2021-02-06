6892b25e426a5318a13c7be9461e0c1a530ec28a
jossrc <kuro.rc10@gmail.com>
2021-02-06 09:08:34 -0500

    Recibir información desde la línea de comandos

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,8 +1,11 @@
 const { createFile } = require('./helpers/multiply');
 
 console.clear();
-const base = 5;
+//const base = 5;
+
+const [, , arg3 = 'base=5'] = process.argv;
+const [, base = 5] = arg3.split('=');
 
 createFile(base)
-  .then( file =>  console.log(file, ' creado'))
-  .catch(err => console.log(err));
+  .then((file) => console.log(file, ' creado'))
+  .catch((err) => console.log(err));
