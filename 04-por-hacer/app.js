1c34d15699e2ef95d15dca772c1f6ac60127085d
jossrc <kuro.rc10@gmail.com>
2021-02-08 10:06:36 -0500

    Repetir el menu de forma infinita

--- 04-por-hacer/app.js
+++ 04-por-hacer/app.js
@@ -4,8 +4,13 @@ require('colors');
 
 const main = async () => {
   console.log('Hola Mundo');
-  showMenu();
-  pause();
+
+  let answer = '';
+
+  do {
+    answer = await showMenu();
+    if (answer !== '0') await pause();
+  } while (answer !== '0');
 };
 
 main();
