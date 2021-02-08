7d4a3d0e939196336f58079eb008d1504142a366
jossrc <kuro.rc10@gmail.com>
2021-02-08 13:47:29 -0500

    Opciones del menú interactivo

--- 04-por-hacer/helpers/inquirer.js
+++ 04-por-hacer/helpers/inquirer.js
@@ -1,16 +1,44 @@
 const inquirer = require('inquirer');
+
 require('colors');
+
+const choices = [
+  {
+    value: '1',
+    name: '1. Crear tarea',
+  },
+  {
+    value: '2',
+    name: '2. Listar tareas',
+  },
+  {
+    value: '3',
+    name: '3. Listar tareas completadas',
+  },
+  {
+    value: '4',
+    name: '4. Listar tareas pendientes',
+  },
+  {
+    value: '5',
+    name: '5. Completar tarea(s)',
+  },
+  {
+    value: '6',
+    name: '6. Borrar tarea',
+  },
+  {
+    value: '0',
+    name: '0. Salir',
+  },
+];
 
 const questionCollection = [
   {
     type: 'list',
-    name: 'opcion',
+    name: 'option',
     message: '¿Qué desea hacer?',
-    choices: [
-      '1. Crear tarea',
-      '2. Listar tareas',
-      '3. Listar tareas completadas',
-    ],
+    choices,
   },
 ];
 
@@ -20,11 +48,23 @@ const inquirerMenu = async () => {
   console.log('  Seleccione una opción  '.green);
   console.log('=========================\n'.green);
 
-  const options = await inquirer.prompt(questionCollection);
+  const { option } = await inquirer.prompt(questionCollection);
 
-  return options;
+  return option;
+};
+
+const pause = async () => {
+  console.log('\n')
+  await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'continue',
+      message: `Presione ${'ENTER'.green} para continuar...`,
+    },
+  ]);
 };
 
 module.exports = {
   inquirerMenu,
+  pause,
 };
