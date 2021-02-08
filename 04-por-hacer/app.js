1b5c7995a6b55d1fa0b1cb3b56f7fbc4a3fb8ae8
jossrc <kuro.rc10@gmail.com>
2021-02-08 16:29:50 -0500

    Guardando las tareas

--- 04-por-hacer/app.js
+++ 04-por-hacer/app.js
@@ -1,4 +1,5 @@
 const { inquirerMenu, pause, readInput } = require('./helpers/inquirer');
+const { saveDB } = require('./helpers/saveFile');
 const Tasks = require('./models/tasks');
 
 require('colors');
@@ -19,6 +20,8 @@ const main = async () => {
         console.log(tasks.listToArray);
         break;
     }
+
+    saveDB(tasks.listToArray)
 
     await pause();
   } while (answer !== '0');
