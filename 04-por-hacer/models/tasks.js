1098de0ba16c804cedb052ca31479f0eac899c2d
jossrc <kuro.rc10@gmail.com>
2021-02-08 16:02:08 -0500

    Creando get para obtener la lista de tareas como arreglo

--- 04-por-hacer/models/tasks.js
+++ 04-por-hacer/models/tasks.js
@@ -4,10 +4,22 @@ class Tasks {
 
   _list = {};
 
+  /**
+   * @returns {Task[]} Retorna la lista de tareas `_list` como un arreglo.
+   */
+  get listToArray() { 
+    return Object.values(this._list);
+  }
+
   constructor() {
     this._list = {};
   }
 
+  /**
+   * Crea una nueva tarea usando la descripción obtenida. Este
+   * será agregado a la lista de tareas `_list`
+   * @param {string} description Descripción de la tarea.
+   */
   addNewTask(description = '') {
     const task = new Task(description)
     this._list[task.id] = task;
