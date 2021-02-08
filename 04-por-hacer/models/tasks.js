fabefa6e7f8416bc6842a1c226517e76034b0865
jossrc <kuro.rc10@gmail.com>
2021-02-08 15:04:34 -0500

    Creando método para crear y agregar una nueva tarea

--- 04-por-hacer/models/tasks.js
+++ 04-por-hacer/models/tasks.js
@@ -1,9 +1,16 @@
+const Task = require("./task");
+
 class Tasks {
 
   _list = {};
 
   constructor() {
     this._list = {};
+  }
+
+  addNewTask(description = '') {
+    const task = new Task(description)
+    this._list[task.id] = task;
   }
 
 }
