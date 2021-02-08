2a078a040a79ecfecdd248ab7eb6ca3c119b3225
jossrc <kuro.rc10@gmail.com>
2021-02-08 16:01:06 -0500

    Cambiando los números de las elecciones a color verde

--- 04-por-hacer/helpers/inquirer.js
+++ 04-por-hacer/helpers/inquirer.js
@@ -5,31 +5,31 @@ require('colors');
 const choices = [
   {
     value: '1',
-    name: '1. Crear tarea',
+    name: `${'1.'.green} Crear tarea`
   },
   {
     value: '2',
-    name: '2. Listar tareas',
+    name: `${'2.'.green} Listar tareas`
   },
   {
     value: '3',
-    name: '3. Listar tareas completadas',
+    name: `${'3.'.green} Listar tareas completadas`
   },
   {
     value: '4',
-    name: '4. Listar tareas pendientes',
+    name: `${'4.'.green} Listar tareas pendientes`
   },
   {
     value: '5',
-    name: '5. Completar tarea(s)',
+    name: `${'5.'.green} Completar tarea(s)`
   },
   {
     value: '6',
-    name: '6. Borrar tarea',
+    name: `${'6.'.green} Borrar tarea`
   },
   {
     value: '0',
-    name: '0. Salir',
+    name: `${'0.'.green} Salir`
   },
 ];
 
@@ -50,7 +50,7 @@ const questionCollection = [
 const inquirerMenu = async () => {
   console.clear();
   console.log('========================='.green);
-  console.log('  Seleccione una opción  '.green);
+  console.log('  Seleccione una opción  '.white);
   console.log('=========================\n'.green);
 
   const { option } = await inquirer.prompt(questionCollection);
@@ -76,7 +76,7 @@ const pause = async () => {
 
 /**
  * Imprime en pantalla el texto deseado. La respuesta es de
- * caracter obligatorio, este será leída y se retornará
+ * caracter obligatorio, esta será leída y se retornará
  * como una Promesa.
  * @param {string} message Mensaje a imprimir.
  * @return {Promise<string>} Respuesta al mensaje impreso.
