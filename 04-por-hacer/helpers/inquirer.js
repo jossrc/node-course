72ec59782e9dcbce3e930db3bf7ad642f94d8f63
jossrc <kuro.rc10@gmail.com>
2021-02-08 11:09:23 -0500

    Creando un pequeño menu con el inquirer

--- 04-por-hacer/helpers/inquirer.js
+++ 04-por-hacer/helpers/inquirer.js
@@ -1,3 +1,30 @@
 const inquirer = require('inquirer');
 require('colors');
 
+const questionCollection = [
+  {
+    type: 'list',
+    name: 'opcion',
+    message: '¿Qué desea hacer?',
+    choices: [
+      '1. Crear tarea',
+      '2. Listar tareas',
+      '3. Listar tareas completadas',
+    ],
+  },
+];
+
+const inquirerMenu = async () => {
+  console.clear();
+  console.log('========================='.green);
+  console.log('  Seleccione una opción  '.green);
+  console.log('=========================\n'.green);
+
+  const options = await inquirer.prompt(questionCollection);
+
+  return options;
+};
+
+module.exports = {
+  inquirerMenu,
+};
