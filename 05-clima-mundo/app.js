110aec7871ae4c500d9569a112a76f7baef720dd
jossrc <kuro.rc10@gmail.com>
2021-02-10 12:01:59 -0500

    Implementando el menu de opciones

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,8 +1,23 @@
-const { readInput } = require("./helpers/inquirer")
+const { readInput, inquirerMenu, pause } = require("./helpers/inquirer")
 
 const main = async () => {
-  const text = await readInput('Ingrese :');
-  console.log(text);
+  
+  let option = 0;
+
+  do {
+    option = await inquirerMenu();
+    switch (option) {
+      case 1:
+        console.log('Buscando ciudad');       
+        break;
+      case 2:
+        console.log('Viendo el historial');
+        break;
+    }
+
+    if (option !== 0) await pause();
+
+  } while (option !== 0);
 }
 
 main();
