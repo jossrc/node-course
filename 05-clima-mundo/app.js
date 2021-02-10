65058a2d3b8f6e872ca06ee0715e09ecc19d851b
jossrc <kuro.rc10@gmail.com>
2021-02-10 12:27:40 -0500

    Modelo para controlar la aplicación

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,6 +1,9 @@
-const { readInput, inquirerMenu, pause } = require("./helpers/inquirer")
+const { readInput, inquirerMenu, pause } = require("./helpers/inquirer");
+const Searches = require("./models/searches");
 
 const main = async () => {
+
+  const searches = new Searches();
   
   let option = 0;
 
@@ -8,7 +11,23 @@ const main = async () => {
     option = await inquirerMenu();
     switch (option) {
       case 1:
-        console.log('Buscando ciudad');       
+        // Mostrar mensaje
+        const city = await readInput('Ciudad: ')
+        console.log(`Buscando ciudad ${city}` );
+        // Buscar los lugares
+
+        // Seleccionar el lugar
+
+        // Clima
+
+        // Mostrar resultados
+        console.log('\nInformación de la ciudad\n'.green);
+        console.log('Ciudad:');
+        console.log('Lat:');
+        console.log('Lng:');
+        console.log('Temperatura:');
+        console.log('Mínima');
+        console.log('Máxima');
         break;
       case 2:
         console.log('Viendo el historial');
