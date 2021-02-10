2aeebb848d7f67ab6b514c3634994c68ae7fbb18
jossrc <kuro.rc10@gmail.com>
2021-02-10 12:27:22 -0500

    Crando modelo para las busquedas

---
+++ 05-clima-mundo/models/searches.js
@@ -0,0 +1,17 @@
+
+class Searches {
+
+  historial = ['Lima', 'Madrid', 'Bogota']
+
+  constructor() {
+    // TODO: Leer DB si existe
+  }
+
+  async city( place = '' ) {
+    // TODO: Petición http
+    return []; // Retorna los lugares que coincidan
+  }
+
+}
+
+module.exports = Searches
