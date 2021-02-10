9ec9f385ad9a097f10ba6d034b7dbcc7dabe0f7e
jossrc <kuro.rc10@gmail.com>
2021-02-10 15:54:04 -0500

    Mostrando en pantalla información de un lugar

--- 05-clima-mundo/app.js
+++ 05-clima-mundo/app.js
@@ -1,33 +1,36 @@
-const { readInput, inquirerMenu, pause } = require("./helpers/inquirer");
-const Searches = require("./models/searches");
+const {
+  readInput,
+  inquirerMenu,
+  pause,
+  showFoundPlaces,
+} = require('./helpers/inquirer');
+const Searches = require('./models/searches');
 
 const main = async () => {
-
   const searches = new Searches();
-  
+
   let option = 0;
 
   do {
     option = await inquirerMenu();
     switch (option) {
       case 1:
-        // Mostrar mensaje
-        const city = await readInput('Ciudad: ')
-        console.log(`Buscando ciudad ${city}` );
-        // Buscar los lugares
+        const result = await readInput('Ciudad: ');
+        const places = await searches.getCity(result);
+        const placeId = await showFoundPlaces(places);
 
-        // Seleccionar el lugar
+        if (placeId !== 0) {
+          const selectedPlace = places.find((place) => place.id === placeId);
 
-        // Clima
-
-        // Mostrar resultados
-        console.log('\nInformación de la ciudad\n'.green);
-        console.log('Ciudad:');
-        console.log('Lat:');
-        console.log('Lng:');
-        console.log('Temperatura:');
-        console.log('Mínima');
-        console.log('Máxima');
+          // Mostrar resultados
+          console.log('\nInformación de la ciudad\n'.green);
+          console.log(`Ciudad: ${selectedPlace.name}`);
+          console.log(`Lat: ${selectedPlace.lat}`);
+          console.log(`Lng: ${selectedPlace.lng}`);
+          console.log('Temperatura:');
+          console.log('Mínima');
+          console.log('Máxima');
+        }
         break;
       case 2:
         console.log('Viendo el historial');
@@ -35,8 +38,7 @@ const main = async () => {
     }
 
     if (option !== 0) await pause();
-
   } while (option !== 0);
-}
+};
 
 main();
