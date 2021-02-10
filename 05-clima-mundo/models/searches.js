876dcfe20c7da752ebe42a70f2ab2e153f2b22f2
jossrc <kuro.rc10@gmail.com>
2021-02-10 15:51:55 -0500

    Creando instancia para obtener la data de la api mapbox

--- 05-clima-mundo/models/searches.js
+++ 05-clima-mundo/models/searches.js
@@ -17,7 +17,19 @@ class Searches {
     // TODO: Leer DB si existe
   }
 
-  async getCity(place = '') {}
+  async getCity(place = '') {
+    try {
+      const instance = axios.create({
+        baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json`,
+        params: this.paramsMapbox,
+      });
+      console.log(instance);
+      return []
+    } catch (error) {
+      console.log('Hubo un error' + error);
+      return [];
+    }
+  }
 }
 
 module.exports = Searches;
