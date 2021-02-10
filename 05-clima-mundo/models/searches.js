7f85cb903e3b7db74b88c98ab2709f668edc3742
jossrc <kuro.rc10@gmail.com>
2021-02-10 15:52:32 -0500

    Trabajando con la data y retornando información básica

--- 05-clima-mundo/models/searches.js
+++ 05-clima-mundo/models/searches.js
@@ -23,8 +23,15 @@ class Searches {
         baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json`,
         params: this.paramsMapbox,
       });
-      console.log(instance);
-      return []
+      const response = await instance.get();
+      const basicInformation = response.data.features.map((place) => ({
+        id: place.id,
+        name: place.place_name,
+        lng: place.center[0],
+        lat: place.center[1],
+      }));
+
+      return basicInformation;
     } catch (error) {
       console.log('Hubo un error' + error);
       return [];
