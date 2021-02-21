72892d0f63a8f2f48ad5e8a66c12d9eb6e4852a2
jossrc <kuro.rc10@gmail.com>
2021-02-21 09:11:42 -0500

    Aplicando formato

--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -2,56 +2,54 @@ require('dotenv').config();
 const express = require('express');
 
 class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
 
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT;
+    // Middlewares
+    this.middlewares();
 
-        // Middlewares
-        this.middlewares();
+    this.routes();
+  }
 
-        this.routes();
-    }
+  middlewares() {
+    // Directorio Público
+    this.app.use(express.static('public'));
+  }
 
-    middlewares() {
-        // Directorio Público
-        this.app.use( express.static('public') )
-    }
+  routes() {
+    this.app.get('/api', (req, res) => {
+      // res.send('Hello World API');
+      res.json({
+        // ok: true, -> Status 200
+        message: 'GET API',
+      });
+    });
 
-    routes() {
-        this.app.get('/api', (req, res) => {
-            // res.send('Hello World API');
-            res.json({
-                // ok: true, -> Status 200
-                message: 'GET API'
-            })
-        });
+    this.app.put('/api', (req, res) => {
+      res.json({
+        message: 'PUT API',
+      });
+    });
 
-        this.app.put('/api', (req, res) => {
-            res.json({
-                message: 'PUT API'
-            })
-        });
+    this.app.post('/api', (req, res) => {
+      res.json({
+        message: 'POST API',
+      });
+    });
 
-        this.app.post('/api', (req, res) => {
-            res.json({
-                message: 'POST API'
-            })
-        });
+    this.app.delete('/api', (req, res) => {
+      res.json({
+        message: 'DELETE API',
+      });
+    });
+  }
 
-        this.app.delete('/api', (req, res) => {
-            res.json({
-                message: 'DELETE API'
-            })
-        });
-
-    }
-
-    listen() {
-        this.app.listen(this.port, () => {
-            console.log(`Escuchando cambios en el port ${this.port}`)
-        });
-    }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Escuchando cambios en el port ${this.port}`);
+    });
+  }
 }
 
 module.exports = Server;
