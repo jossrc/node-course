104e9abf80efd65d8094d8723af65a61b85f3735
jossrc <kuro.rc10@gmail.com>
2021-02-22 09:52:26 -0500

    Habilitando el envío y lectura de datos JSON

--- 07-restserver/models/server.js
+++ 07-restserver/models/server.js
@@ -19,6 +19,9 @@ class Server {
     // CORS
     this.app.use(cors());
 
+    // Lectura y parseo del body
+    this.app.use(express.json());
+
     // Directorio Público
     this.app.use(express.static('public'));
   }
