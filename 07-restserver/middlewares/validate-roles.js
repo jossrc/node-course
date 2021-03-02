929c97a3de35a96ac8bfb7f85db858f5406a7f56
jossrc <kuro.rc10@gmail.com>
2021-03-02 08:59:53 -0500

    Aplicando formato al middleware

--- 07-restserver/middlewares/validate-roles.js
+++ 07-restserver/middlewares/validate-roles.js
@@ -1,42 +1,38 @@
 const { response, request } = require('express');
 
 const verifyAdminRole = (req = request, res = response, next) => {
+  if (!req.authenticatedUser)
+    return res.status(500).json({
+      message: 'Se requiere verificar el rol sin validar el token primero',
+    });
 
-    if (!req.authenticatedUser)
-        return res.status(500).json({
-            message: 'Se requiere verificar el rol sin validar el token primero'
-        });
+  const { role, name } = req.authenticatedUser;
 
-    const { role, name } = req.authenticatedUser;
+  if (role !== 'ADMIN_ROLE')
+    return res.status(401).json({
+      message: `${name} no es administrador - No puede hacer esto`,
+    });
 
-    if ( role !== 'ADMIN_ROLE' )
-        return res.status(401).json({
-            message: `${name} no es administrador - No puede hacer esto`
-        });
-
-    next();
-
-}
+  next();
+};
 
 const existsRole = (...roles) => {
-    return (req = request, res = response, next) => {
+  return (req = request, res = response, next) => {
+    if (!req.authenticatedUser)
+      return res.status(500).json({
+        message: 'Se requiere verificar el rol sin validar el token primero',
+      });
 
-        if (!req.authenticatedUser)
-            return res.status(500).json({
-                message: 'Se requiere verificar el rol sin validar el token primero'
-            });
+    if (!roles.includes(req.authenticatedUser.role))
+      return res.status(401).json({
+        message: `El servicio requiere uno de estos roles ${roles}`,
+      });
 
-        if (!roles.includes(req.authenticatedUser.role))
-            return res.status(401).json({
-                message: `El servicio requiere uno de estos roles ${roles}`
-            })
-
-        next();
-    }
-}
+    next();
+  };
+};
 
 module.exports = {
-    verifyAdminRole,
-    existsRole
-}
-
+  verifyAdminRole,
+  existsRole,
+};
