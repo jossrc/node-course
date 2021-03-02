fc7f891e1d25af543a19e5008eece236e1646d0a
jossrc <kuro.rc10@gmail.com>
2021-03-02 14:39:21 -0500

    Creando helper para validar el token de google

---
+++ 07-restserver/helpers/google-verify.js
@@ -0,0 +1,21 @@
+const { OAuth2Client } = require('google-auth-library');
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+const googleVerify = async (idToken = '') => {
+  const ticket = await client.verifyIdToken({
+    idToken,
+    audience: process.env.GOOGLE_CLIENT_ID,
+  });
+
+  const { name, picture: img, email } = ticket.getPayload();
+  return {
+    name,
+    img,
+    email,
+  };
+};
+
+module.exports = {
+  googleVerify,
+};
