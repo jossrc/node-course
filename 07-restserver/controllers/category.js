8e20d2c718807590335c808ccca372ad18e9ee01
jossrc <kuro.rc10@gmail.com>
2021-03-03 17:07:13 -0500

    Creando métodos CRUD en el controller de la categoría

--- 07-restserver/controllers/category.js
+++ 07-restserver/controllers/category.js
@@ -1,6 +1,51 @@
 const { request, response } = require('express');
 const { Category } = require('../models');
 
+// getCategories - paginado - total - populate
+// getCategory - populate {}
+
+const getCategories = async (req = request, res = response) => {
+  const { since = 0, limit = 5 } = req.query;
+  const filter = { state: true };
+
+  if (isNaN(Number(since)) || isNaN(Number(limit)))
+    return res.status(400).json({
+      message: 'Ingrese un valor inicial y/o limite válido',
+    });
+
+  const total = await Category.countDocuments(filter);
+
+  if (since >= total)
+    return res.status(400).json({
+      message: `Ingrese un valor inicial (${since}) menor al total: ${total}`,
+    });
+
+  const categories = await Category.find(filter)
+    .skip(Number(since))
+    .limit(Number(limit))
+    .populate('user', {
+      name: true,
+      uid: true,
+    });
+
+  res.json({
+    message: 'Categoría(s) obtenida(s) correctamente',
+    total,
+    categories,
+  });
+};
+
+const getCategory = async (req = request, res = response) => {
+  const { id: categoryID } = req.params;
+
+  const category = await Category.findById(categoryID).populate('user');
+
+  res.json({
+    message: 'Categoría obtenida',
+    category,
+  });
+};
+
 const createCategory = async (req = request, res = response) => {
   const name = req.body.name.trim().toUpperCase();
   const categoryDB = await Category.findOne({ name });
@@ -27,6 +72,45 @@ const createCategory = async (req = request, res = response) => {
   });
 };
 
+const updateCategory = async (req = request, res = response) => {
+  // const authenticatedUser = req.authenticatedUser;
+  const { id: categoryID } = req.params;
+  const { name } = req.body;
+
+  // Obtiene la categoría actualizada
+  const category = await Category.findByIdAndUpdate(
+    categoryID,
+    { name },
+    { new: true }
+  );
+
+  res.json({
+    message: 'Categoría actualizada correctamente',
+    category,
+  });
+};
+
+// deleteCategory - estado : false
+const deleteCategory = async (req = request, res = response) => {
+  const { id: categoryID } = req.params;
+
+  const category = await Category.findByIdAndUpdate(
+      categoryID,
+      { state: false },
+      { new: true }
+  );
+
+  res.json({
+    message: 'La categoría se eliminó correctamente',
+    category,
+  });
+}
+
+
 module.exports = {
-  createCategory: createCategory,
+  createCategory,
+  getCategories,
+  getCategory,
+  updateCategory,
+  deleteCategory
 };
