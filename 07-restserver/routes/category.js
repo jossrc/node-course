8714294672a84a4c432655c74594d7321d452724
jossrc <kuro.rc10@gmail.com>
2021-03-03 10:23:00 -0500

    Validando y agregando el controller para la creación de una categoría

--- 07-restserver/routes/category.js
+++ 07-restserver/routes/category.js
@@ -1,43 +1,48 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { dataValidator } = require('../middlewares/dataValidator');
+const { validateJWT, dataValidator } = require('../middlewares');
+const { createCategory } = require('../controllers/category');
 
 const router = Router();
 
 // Obtener todas las categorías - publico
 router.get('/', (req, res) => {
-    res.json({
-        message: 'Todo ok - GET'
-    });
-})
+  res.json({
+    message: 'Todo ok - GET',
+  });
+});
 
 // Obtener una categoría por id - público
 router.get('/:id', (req, res) => {
-    res.json({
-        message: 'Todo ok - GET - ID'
-    });
-})
+  res.json({
+    message: 'Todo ok - GET - ID',
+  });
+});
 
 // Crear categoría - privado - cualquier persona con un token válido
-router.post('/', (req, res) => {
-    res.json({
-        message: 'Todo ok - POST'
-    });
-})
+router.post(
+  '/',
+  [
+    validateJWT,
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    dataValidator,
+  ],
+  createCategory
+);
 
 // Actualizar - privado - cualquiera con token válido
 router.put('/:id', (req, res) => {
-    res.json({
-        message: 'Todo ok - PUT'
-    });
-})
+  res.json({
+    message: 'Todo ok - PUT',
+  });
+});
 
 // Borrar una categoría - Admin
 router.delete('/:id', (req, res) => {
-    res.json({
-        message: 'Todo ok - PUT - DELETE'
-    });
-})
+  res.json({
+    message: 'Todo ok - PUT - DELETE',
+  });
+});
 
 module.exports = router;
