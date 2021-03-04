b037230191c77753660ef60410c91f26c7bce279
jossrc <kuro.rc10@gmail.com>
2021-03-04 13:43:38 -0500

    Validando los campos y estableciendo los controllers a las rutas del producto

--- 07-restserver/routes/product.js
+++ 07-restserver/routes/product.js
@@ -1,65 +1,91 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 
-const { validateJWT, dataValidator, existsRole, verifyAdminRole } = require('../middlewares');
 const {
-    getProducts,
-    getProduct,
-    createProduct,
-    updateProduct,
-    deleteProduct
+  validateJWT,
+  dataValidator,
+  existsRole,
+  verifyAdminRole,
+} = require('../middlewares');
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 } = require('../controllers/product.js');
+
+const { existsProduct, existsCategory } = require('../helpers/db-validators');
 
 const router = Router();
 
 router.get('/', getProducts);
 
 router.get(
-    '/:id',
-    [
-        check('id', 'No es un id de Mongo válido').isMongoId(),
-        check('id').custom(async (id) => {
-            //await existsCategory(id);
-        }),
-        dataValidator,
-    ],
-    getProduct
+  '/:id',
+  [
+    check('id', 'No es un id de Mongo válido').isMongoId(),
+    check('id').custom(async (id) => {
+      await existsProduct(id);
+    }),
+    dataValidator,
+  ],
+  getProduct
 );
 
 router.post(
-    '/',
-    [
-        validateJWT,
-        existsRole('ADMIN_ROLE', 'SALES_ROLE'),
-        check('name', 'El nombre es obligatorio').not().isEmpty(),
-        dataValidator,
-    ],
-    createProduct
+  '/',
+  [
+    validateJWT,
+    existsRole('ADMIN_ROLE', 'SALES_ROLE'),
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('price', 'El precio debe ser un número').isNumeric(),
+    check(
+      'category',
+      'La categoría no tiene un id de Mongo válido'
+    ).isMongoId(),
+    check('category').custom(async (category) => {
+      await existsCategory(category);
+    }),
+    dataValidator,
+  ],
+  createProduct
 );
 
 router.put(
-    '/:id',
-    [
-        validateJWT,
-        existsRole('ADMIN_ROLE', 'SALES_ROLE'),
-        check('id', 'No es un id de Mongo válido').isMongoId(),
-        check('id').custom(async (id) => {
-           // await existsCategory(id);
-        }),
-        check('name', 'El nombre es obligatorio').not().isEmpty(),
-        dataValidator,
-    ],
-    updateProduct
+  '/:id',
+  [
+    validateJWT,
+    existsRole('ADMIN_ROLE', 'SALES_ROLE'),
+    check('id', 'No es un id de Mongo válido').isMongoId(),
+    check('id').custom(async (id) => {
+      await existsProduct(id);
+    }),
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check(
+      'category',
+      'La categoría no tiene un id de Mongo válido'
+    ).isMongoId(),
+    check('category').custom(async (category) => {
+      await existsCategory(category);
+    }),
+    dataValidator,
+  ],
+  updateProduct
 );
 
-router.delete('/:id', [
+router.delete(
+  '/:id',
+  [
     validateJWT,
     verifyAdminRole,
     check('id', 'No es un id de Mongo válido').isMongoId(),
     check('id').custom(async (id) => {
-        //await existsCategory(id);
+      await existsProduct(id);
     }),
     dataValidator,
-] , deleteProduct);
+  ],
+  deleteProduct
+);
 
 module.exports = router;
