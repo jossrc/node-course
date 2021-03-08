4dc450b7568c226ebbef1c8d5910329d7e5b42d2
jossrc <kuro.rc10@gmail.com>
2021-03-08 16:28:04 -0500

    Crear controller para subir archivos

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -1,13 +1,29 @@
+const path = require('path');
 const { request, response } = require('express');
 
 const uploadFiles = (req = request, res = response) => {
+  if (!req.files || Object.keys(req.files).length === 0 || !req.files.myFile) {
+    return res.status(400).json({
+      message: 'No hay archivos que subir',
+    });
+  }
+
+  const { myFile } = req.files;
+  const uploadPath = path.join(__dirname, '../uploads', myFile.name);
+
+  myFile.mv(uploadPath, (err) => {
+    if (err) {
+      return res.status(500).json({
+        err,
+      });
+    }
 
     res.json({
-        message: 'Archivo cargado correctamente'
-    })
-
-}
+      message: 'Archivo subido a ' + uploadPath,
+    });
+  });
+};
 
 module.exports = {
-    uploadFiles
-}
+  uploadFiles,
+};
