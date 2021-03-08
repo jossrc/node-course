c242839bf0b10f040cf2d197d979c15d966c8a56
jossrc <kuro.rc10@gmail.com>
2021-03-08 11:05:08 -0500

    Búsquedas en base de datos

--- 07-restserver/controllers/search.js
+++ 07-restserver/controllers/search.js
@@ -1,14 +1,49 @@
-const {request, response} = require('express');
+const { request, response } = require('express');
+const { ObjectId } = require('mongoose').Types;
+const { User } = require("../models");
+
+const allowedCollections = [
+    'usuarios',
+    'categoria',
+    'productos',
+    'roles'
+];
+
+const searchUsers = async (term = '', res = response)=> {
+    const isMongoId = ObjectId.isValid(term);
+
+    if (isMongoId) {
+        const user = await User.findById(term);
+        res.json({
+            results: ( user ? [user] : [] )
+        })
+    }
+
+}
 
 const search = (req = request, res = response) => {
 
-    const { collection, term } = req.params;
+    const {collection, term} = req.params;
 
-    res.json({
-        message: 'Buscar',
-        collection,
-        term
-    });
+    if (!allowedCollections.includes(collection))
+        return res.status(400).json({
+            message: `Las colecciones permitidas son: ${allowedCollections}`
+        })
+
+    switch (collection) {
+        case 'usuarios':
+            searchUsers(term, res)
+            break;
+        case 'categoria':
+            break;
+        case 'productos':
+            break;
+        default:
+            res.status(500).json({
+                message: 'Esta búsqueda no se encuentra disponible'
+            })
+    }
+
 }
 
 module.exports = {
