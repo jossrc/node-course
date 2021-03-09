5158ea14d7c25c48a0eba2c39ff0d0600e25b66a
jossrc <kuro.rc10@gmail.com>
2021-03-09 12:30:05 -0500

    Usar helper para subir archivos

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -1,41 +1,20 @@
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
 const { request, response } = require('express');
+const { uploadYourFile  } = require('../helpers');
 
-const uploadFiles = (req = request, res = response) => {
+const uploadFiles = async (req = request, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.myFile) {
     return res.status(400).json({
       message: 'No hay archivos que subir',
     });
   }
 
-  const { myFile } = req.files;
-  const cutFileName = myFile.name.toLowerCase().split('.');
-  const fileExtension = cutFileName[cutFileName.length - 1];
+  const fileName = await uploadYourFile(req.files);
 
-  // Validar la extensión
-  const validFileExtensions = ['png', 'jpg', 'jpeg', 'gif', 'webp'];
-  if (!validFileExtensions.includes(fileExtension)) {
-    return res.status(400).json({
-      message: `La extensión ${fileExtension} no es permitida, ${validFileExtensions}`
-    })
-  }
+  res.json({
+    message: `El archivo ${fileName} se subió correctamente`,
+    fileName
+  })
 
-  const newUniqueFileName = `${uuidv4()}.${fileExtension}`;
-
-  const uploadPath = path.join(__dirname, '../uploads', newUniqueFileName);
-
-  myFile.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({
-        err,
-      });
-    }
-
-    res.json({
-      message: 'Archivo subido a ' + uploadPath,
-    });
-  });
 };
 
 module.exports = {
