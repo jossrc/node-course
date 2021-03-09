2293aa16211b18ecfd0aa01f9e7220810a932c20
jossrc <kuro.rc10@gmail.com>
2021-03-09 15:58:26 -0500

    Crear controller de actualizar imagen

--- 07-restserver/controllers/uploads.js
+++ 07-restserver/controllers/uploads.js
@@ -1,13 +1,9 @@
 const { request, response } = require('express');
 const { uploadYourFile } = require('../helpers');
 
-const uploadFiles = async (req = request, res = response) => {
-  if (!req.files || Object.keys(req.files).length === 0 || !req.files.myFile) {
-    return res.status(400).json({
-      message: 'No hay archivos que subir',
-    });
-  }
+const { User, Product } = require('../models');
 
+const uploadFiles = async (req = request, res = response) => {
   try {
     // const fileName = await uploadYourFile(req.files, ['txt', 'md'], 'texts');
     const fileName = await uploadYourFile(req.files, undefined, 'imgs');
@@ -22,16 +18,44 @@ const uploadFiles = async (req = request, res = response) => {
 };
 
 const updateImage = async (req = request, res = response) => {
-
   const { id, collection } = req.params;
 
+  let model;
+
+  switch (collection) {
+    case 'usuarios':
+      model = await User.findById(id);
+      if (!model) {
+        return res.status(400).json({
+          message: `No existe un usuario con el id ${id}`,
+        });
+      }
+      break;
+    case 'productos':
+      model = await Product.findById(id);
+      if (!model) {
+        return res.status(400).json({
+          message: `No existe un producto con el id ${id}`,
+        });
+      }
+      break;
+    default:
+      return res.status(500).json({
+        message: 'Colección en proceso de validación...',
+      });
+  }
+
+  model.img = await uploadYourFile(req.files, undefined, collection);
+
+  await model.save();
+
   res.json({
-    id,
-    collection
-  })
-}
+    message: 'Se actualizó correctamente la imagen',
+    model,
+  });
+};
 
 module.exports = {
   uploadFiles,
-  updateImage
+  updateImage,
 };
