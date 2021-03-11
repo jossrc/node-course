e4f627c92454861071f8ceaedc4b491a7fe21793
jossrc <kuro.rc10@gmail.com>
2021-03-11 12:04:43 -0500

    Configuración de socket.io

--- 08-websocket-server/models/server.js
+++ 08-websocket-server/models/server.js
@@ -15,6 +15,9 @@ class Server {
 
     // Rutas de mi aplicación
     this.routes();
+
+    // Sockets
+    this.sockets();
   }
 
   middlewares() {
@@ -27,6 +30,15 @@ class Server {
 
   routes() {
     // this.app.use( this.paths.auth, require('../routes/auth'));
+  }
+
+  sockets() {
+    this.io.on('connection', (socket) => {
+      console.log('Cliente conectado ', socket.id);
+      socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+      });
+    });
   }
 
   listen() {
