b1af401732b95bd47fcce3d4f8f7b5be32f161c4
jossrc <kuro.rc10@gmail.com>
2021-03-11 14:56:38 -0500

    Mensajes de conexión y desconexión - cliente

--- 08-websocket-server/public/js/socket-client.js
+++ 08-websocket-server/public/js/socket-client.js
@@ -1,3 +1,16 @@
-console.log('Hello World');
+// Referencias del HTML
+const lblOnline = document.getElementById('lblOnline');
+const lblOffline = document.getElementById('lblOffline');
 
 const socketClient = io();
+
+socketClient.on('connect', () => {
+   lblOnline.style.display = '';
+   lblOffline.style.display = 'none';
+});
+
+socketClient.on('disconnect', () => {
+    lblOffline.style.display = '';
+    lblOnline.style.display = 'none';
+});
+
