4ada1a21696750136ec5d7d958e1f0b9c301a37c
jossrc <kuro.rc10@gmail.com>
2021-03-11 16:52:58 -0500

    Emitir desde el cliente

--- 08-websocket-server/public/js/socket-client.js
+++ 08-websocket-server/public/js/socket-client.js
@@ -1,16 +1,28 @@
 // Referencias del HTML
 const lblOnline = document.getElementById('lblOnline');
 const lblOffline = document.getElementById('lblOffline');
+const txtMessage = document.getElementById('txtMessage');
+const btnSend = document.getElementById('btnSend');
 
 const socketClient = io();
 
 socketClient.on('connect', () => {
-   lblOnline.style.display = '';
-   lblOffline.style.display = 'none';
+  lblOnline.style.display = '';
+  lblOffline.style.display = 'none';
 });
 
 socketClient.on('disconnect', () => {
-    lblOffline.style.display = '';
-    lblOnline.style.display = 'none';
+  lblOffline.style.display = '';
+  lblOnline.style.display = 'none';
 });
 
+btnSend.addEventListener('click', () => {
+  const message = txtMessage.value;
+  const payload = {
+    message,
+    id: '123ABC',
+    date: new Date().getTime(),
+  };
+
+  socketClient.emit('send-message', payload);
+});
