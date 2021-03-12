236537e04c9a54b1c8be95011ec65ba970b8adc2
jossrc <kuro.rc10@gmail.com>
2021-03-12 11:34:17 -0500

    Escuchando y emitiendo el ticket

--- 09-socket-colas/sockets/controller.js
+++ 09-socket-colas/sockets/controller.js
@@ -3,11 +3,13 @@ const TicketControl = require('../models/ticket-control');
 const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
-  socket.on('enviar-mensaje', (payload, callback) => {
-    const id = 123456789;
-    callback(id);
 
-    socket.broadcast.emit('enviar-mensaje', payload);
+  socket.emit('ultimo-ticket', ticketControl.ultimo);
+
+  socket.on('siguiente-ticket', (payload, callback) => {
+    const siguiente = ticketControl.siguiente();
+    callback(siguiente);
+    // TODO: Notificar que hay un nuevo ticket pendiente de asignar
   });
 };
 
