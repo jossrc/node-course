2bf0840048e6fabe40ab64942a3885c7c877df64
jossrc <kuro.rc10@gmail.com>
2021-03-12 12:29:39 -0500

    Preparar pantalla de escritorio

--- 09-socket-colas/public/js/escritorio.js
+++ 09-socket-colas/public/js/escritorio.js
@@ -1,1 +1,33 @@
-console.log('Escritorio HTML');
+const lblTituloEscritorio = document.getElementById('lblTituloEscritorio');
+const btnAtenderTicket = document.getElementById('btnAtenderTicket');
+
+const searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html'
+    throw new Error('El escritorio es obligatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+lblTituloEscritorio.textContent = escritorio;
+
+const socket = io();
+
+socket.on('connect', () => {
+    btnAtenderTicket.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnAtenderTicket.disabled = true;
+});
+
+socket.on('ultimo-ticket', (ultimoTicket)=> {
+   // lblNuevoTicket.textContent = ultimoTicket;
+})
+
+btnAtenderTicket.addEventListener('click', ()=> {
+    // socket.emit('siguiente-ticket', null, (ticket) => {
+    //     console.log('Desde el server', ticket)
+    //     lblNuevoTicket.textContent = ticket;
+    // })
+})
