07715d23a3790c6fd84bf9815731c14937588205
jossrc <kuro.rc10@gmail.com>
2021-03-12 11:35:02 -0500

    Crear nuevo ticket

--- 09-socket-colas/public/js/nuevo-ticket.js
+++ 09-socket-colas/public/js/nuevo-ticket.js
@@ -1,1 +1,24 @@
-console.log('Nuevo Ticket HTML');
+const lblNuevoTicket = document.getElementById('lblNuevoTicket');
+const btnNuevoTicket = document.getElementById('btnNuevoTicket');
+
+const socket = io();
+
+socket.on('connect', () => {
+   btnNuevoTicket.disabled = false;
+});
+
+socket.on('disconnect', () => {
+   btnNuevoTicket.disabled = true;
+});
+
+socket.on('ultimo-ticket', (ultimoTicket)=> {
+  lblNuevoTicket.textContent = ultimoTicket;
+})
+
+btnNuevoTicket.addEventListener('click', ()=> {
+    socket.emit('siguiente-ticket', null, (ticket) => {
+        console.log('Desde el server', ticket)
+        lblNuevoTicket.textContent = ticket;
+    })
+})
+
