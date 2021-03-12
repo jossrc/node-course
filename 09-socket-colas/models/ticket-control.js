a11a6fb8ebec76bc760738c93b286b9797bfca12
jossrc <kuro.rc10@gmail.com>
2021-03-12 10:54:57 -0500

    Crear métodos siguiente y atenderTicket - clase Ticket

--- 09-socket-colas/models/ticket-control.js
+++ 09-socket-colas/models/ticket-control.js
@@ -1,6 +1,13 @@
 const path = require('path');
 const fs = require('fs');
 
+class Ticket {
+  constructor(numero, escritorio) {
+    this.numero = numero;
+    this.escritorio = escritorio;
+  }
+}
+
 class TicketControl {
   constructor() {
     this.ultimo = 0;
@@ -35,6 +42,33 @@ class TicketControl {
   guardarDB() {
     const dbPath = path.join(__dirname, '../db/data.json');
     fs.writeFileSync(dbPath, JSON.stringify(this.toJson));
+  }
+
+  siguiente() {
+    this.ultimo += 1;
+    const ticket = new Ticket(this.ultimo, null);
+    this.tickets.push(ticket);
+
+    this.guardarDB();
+    return ticket.numero;
+  }
+
+  atenderTicket(escritorio) {
+    if (this.tickets.length === 0) {
+      return null;
+    }
+
+    const ticket = this.tickets.shift(); //this.tickets[0];
+
+    ticket.escritorio = escritorio;
+    this.ultimos4.unshift(ticket);
+
+    if (this.ultimos4.length > 4) {
+      this.ultimos4.splice(-1, 1);
+    }
+
+    this.guardarDB();
+    return ticket;
   }
 }
 
