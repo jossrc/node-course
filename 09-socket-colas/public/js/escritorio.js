e7fdc555c62900bbd6396fb7f47a1c5ed24646bb
jossrc <kuro.rc10@gmail.com>
2021-03-12 13:07:28 -0500

    Emitir el evento de atender-ticket

--- 09-socket-colas/public/js/escritorio.js
+++ 09-socket-colas/public/js/escritorio.js
@@ -1,5 +1,7 @@
 const lblTituloEscritorio = document.getElementById('lblTituloEscritorio');
 const btnAtenderTicket = document.getElementById('btnAtenderTicket');
+const lblTicket = document.getElementById('lblTicket');
+const divAlerta = document.querySelector(".alert");
 
 const searchParams = new URLSearchParams(window.location.search);
 
@@ -10,6 +12,7 @@ if (!searchParams.has('escritorio')) {
 
 const escritorio = searchParams.get('escritorio');
 lblTituloEscritorio.textContent = escritorio;
+divAlerta.style.display = 'none';
 
 const socket = io();
 
@@ -26,6 +29,14 @@ socket.on('ultimo-ticket', (ultimoTicket)=> {
 })
 
 btnAtenderTicket.addEventListener('click', ()=> {
+    socket.emit('atender-ticket', { escritorio }, ({ ok, ticket, msg }) => {
+        if (!ok) {
+            lblTicket.textContent = 'Nadie';
+            return divAlerta.style.display = '';
+        }
+
+        lblTicket.textContent = `Ticket ${ticket.numero}`
+    })
     // socket.emit('siguiente-ticket', null, (ticket) => {
     //     console.log('Desde el server', ticket)
     //     lblNuevoTicket.textContent = ticket;
