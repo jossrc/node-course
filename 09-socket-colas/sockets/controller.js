e9b3da5cbf366ae5261ee1455f2782dba5046573
jossrc <kuro.rc10@gmail.com>
2021-03-12 07:17:06 -0500

    Actualizar controller

--- 09-socket-colas/sockets/controller.js
+++ 09-socket-colas/sockets/controller.js
@@ -1,10 +1,4 @@
 const socketController = (socket) => {
-  console.log('Cliente conectado', socket.id);
-
-  socket.on('disconnect', () => {
-    console.log('Cliente desconectado', socket.id);
-  });
-
   socket.on('enviar-mensaje', (payload, callback) => {
     const id = 123456789;
     callback(id);
