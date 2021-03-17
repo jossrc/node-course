822506d2b9db4190ac4c36aea07fadaeae9d0026
jossrc <kuro.rc10@gmail.com>
2021-03-17 11:58:04 -0500

    Implementar las rutas

--- 11-ts-restserver/models/server.ts
+++ 11-ts-restserver/models/server.ts
@@ -1,12 +1,23 @@
 import express, { Application } from 'express';
+import userRoutes from '../routes/user';
 
 class Server {
   private app: Application;
   private port: string | number;
+  private apiPaths = {
+    users: '/api/usuarios'
+  }
 
   constructor() {
     this.app = express();
     this.port = process.env.PORT || '8000';
+
+    // Definir mis rutas
+    this.routes();
+  }
+
+  routes() {
+    this.app.use(this.apiPaths.users, userRoutes);
   }
 
   listen() {
