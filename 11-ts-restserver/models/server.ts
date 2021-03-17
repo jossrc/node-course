f0edd089234552449bab9c547b5a4aabe6bfa8df
jossrc <kuro.rc10@gmail.com>
2021-03-17 10:34:00 -0500

    Crear server

---
+++ 11-ts-restserver/models/server.ts
@@ -0,0 +1,19 @@
+import express, { Application } from 'express';
+
+class Server {
+  private app: Application;
+  private port: string | number;
+
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || '8000';
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(`Servidor corriendo en el puerto ${this.port}`);
+    });
+  }
+}
+
+export default Server;
